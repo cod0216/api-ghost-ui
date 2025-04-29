53729bb518f0a3d20f2a1dd2803567bf9bf3a4ef
haelim-kweon <khaelim1311@gmail.com>
2025-04-29 14:30:10 +0900

    feat: set up sidebar for Dashboard page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,12 @@
 import styles from './Dashboard.module.scss';
+import Sidebar from '@/pages/dashboard/sidebar/Sidebar.tsx';
 
 const Dashboard: React.FC = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.sidebar}>sidebar</div>
+      <div className={styles.sidebar}>
+        <Sidebar />
+      </div>
       <div className={styles.mainContent}>main</div>
     </div>
   );
