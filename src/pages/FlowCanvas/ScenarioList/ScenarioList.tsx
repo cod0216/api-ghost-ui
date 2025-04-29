65af2a82621a148c8090648fd55db7a54688835c
cod0216 <cod0216@nate.com>
2025-04-30 01:16:08 +0900

    feat: add ScenarioList UI

---
+++ src/pages/FlowCanvas/ScenarioList/ScenarioList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './ScenarioList.module.scss';
+
+/**
+ * Displays a static list of user scenarios for selection.
+ *
+ * @param onSelect - Handler invoked with the ID of the clicked scenario.
+ */
+const ScenarioList = () => {
+  return <ul className={styles.list}>{}</ul>;
+};
+
+export default ScenarioList;
