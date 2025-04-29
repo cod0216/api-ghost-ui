4efda8645e4cdc01c5381df9589c5e7d15861526
cod0216 <cod0216@nate.com>
2025-04-30 03:20:38 +0900

    feat: mockendpoint list up

--- src/pages/flow-canvas/api-list/api-list.tsx
+++ src/pages/flow-canvas/api-list/api-list.tsx
@@ -1,7 +1,30 @@
+import React from 'react';
 import styles from './api-list.module.scss';
+import { mockEndpoints, Endpoint } from '@/pages/flow-canvas/data/endpoint.ts';
 
-const ApiList = () => {
-  return <ul className={styles.list}></ul>;
+/**
+ * Renders a list of endpoints available for dragging onto the flow canvas.
+ */
+const ApiList: React.FC = () => {
+  const onDragStart = (event: React.DragEvent<HTMLLIElement>, endpoint: Endpoint) => {
+    event.dataTransfer.setData('application/reactflow', JSON.stringify(endpoint));
+    event.dataTransfer.effectAllowed = 'move';
+  };
+
+  return (
+    <ul className={styles.list}>
+      {mockEndpoints.map(endpoint => (
+        <li
+          key={endpoint.id}
+          className={styles.item}
+          draggable
+          onDragStart={e => onDragStart(e, endpoint)}
+        >
+          <span className={styles.method}>{endpoint.method}</span> {endpoint.path}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ApiList;
