67efb2833d8e43bb391ee204bbab15ca6d260f91
haelim-kweon <khaelim1311@gmail.com>
2025-05-09 01:42:01 +0900

    chore: change loadConfig function to arrow function

--- src/config/urlConfig.ts
+++ src/config/urlConfig.ts
@@ -2,10 +2,10 @@ export interface AppConfig {
   baseUrl: string;
 }
 
-export async function loadConfig(): Promise<AppConfig> {
+export const loadConfig = async (): Promise<AppConfig> => {
   const response = await fetch('/apighost-ui/config.json');
   if (!response.ok) {
     throw new Error('Failed to load config');
   }
   return await response.json();
-}
+};
