8b262d51dda4320b4f855b0a332d7d891a10f6c3
cod0216 <cod0216@nate.com>
2025-05-11 03:47:28 +0900

    feat: integrate Redux for flowchart state management

--- src/pages/flow-canvas/FlowCanvasMain.tsx
+++ src/pages/flow-canvas/FlowCanvasMain.tsx
@@ -1,7 +1,15 @@
 import FlowCanvas from '@/pages/flow-canvas/FlowCanvas.tsx';
+import { fetchEndpoints } from '@/store/thunks/fetchEndpoints';
+import { useEffect } from 'react';
 import { ReactFlowProvider } from 'reactflow';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
 
 const FlowCanvasMain = () => {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(fetchEndpoints());
+  }, [dispatch]);
+
   return (
     <ReactFlowProvider>
       <FlowCanvas />
