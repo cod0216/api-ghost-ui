5d17a32805e33cca540118e4ce3a82fbfb375624
cod0216 <cod0216@nate.com>
2025-05-11 02:22:46 +0900

    refactor: add MainTabType and SubTabType

--- src/pages/flow-canvas/types/bodyEditor.ts
+++ src/pages/flow-canvas/types/bodyEditor.ts
@@ -1,18 +1,31 @@
+import { constants } from 'buffer';
+
 export type Tab = { label: string; showSchema?: boolean; showSave?: boolean };
 export type CombineTab = { mainTab: Tab; subTabs: Tab[] };
 export type Path = number[];
 
+export const enum MainTabType {
+  REQUEST = 'Request',
+  RESPONSE = 'Response',
+}
+
+export const enum SubTabType {
+  HEADER = 'Header',
+  BODY = 'Body',
+  PARAMS = 'Params',
+}
+
 export const BODY_EDITOR_TABS: readonly CombineTab[] = [
   {
-    mainTab: { label: 'Request' },
+    mainTab: { label: MainTabType.REQUEST },
     subTabs: [
-      { label: 'Header' },
-      { label: 'Body', showSchema: true, showSave: true },
-      { label: 'Params' },
+      { label: SubTabType.HEADER },
+      { label: SubTabType.BODY, showSchema: true, showSave: true },
+      { label: SubTabType.PARAMS },
     ],
   },
   {
-    mainTab: { label: 'Response' },
-    subTabs: [{ label: 'Header' }, { label: 'Body', showSchema: true }],
+    mainTab: { label: MainTabType.RESPONSE },
+    subTabs: [{ label: SubTabType.HEADER }, { label: SubTabType.BODY, showSchema: true }],
   },
 ] as const satisfies readonly CombineTab[];
