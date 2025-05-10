e8e35cf0a120e724b4770c6529eb5be338f9b34d
cod0216 <cod0216@nate.com>
2025-05-11 03:02:01 +0900

    fix: export Rootstate and AppDispatch, const value name modify

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import endpointReducer from './slices/endpointSlice';
 import nodeReducer from './slices/nodeSlice';
-import bodyEditorReducer from './slices/bodyEditorReducer';
+import bodyEditorReducer from './slices/schemaEditorSlice';
 
-export const stroe = configureStore({
+export const store = configureStore({
   reducer: {
     endPoint: endpointReducer,
     node: nodeReducer,
     bodyEditor: bodyEditorReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
