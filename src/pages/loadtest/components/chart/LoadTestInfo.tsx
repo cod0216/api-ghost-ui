f28d5e73299676eba151b2e553ee39a92b48e3f8
haelim-kweon <khaelim1311@gmail.com>
2025-05-19 05:09:56 +0900

    feat: Create graph component module

---
+++ src/pages/loadtest/components/chart/LoadTestInfo.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import styles from '@/pages/load-test/styles/LoadTest.module.scss';
+import playIcon from '@/assets/icons/play.svg';
+
+import { LoadTestParamInfo } from '@/pages/loadtest/types';
+import ChartArea from './ChartArea';
+import buttonStyles from '@/common/components/SaveButton.module.scss';
+
+interface LoadTestInfoProps {
+  loadTest: LoadTestParamInfo | null;
+}
+
+const LoadTestInfo: React.FC<LoadTestInfoProps> = ({ loadTest }) => {
+  const [onTest, setOnTest] = useState<boolean>(false);
+
+  const handleSelectExcute = () => {
+    if (!loadTest) return;
+    setOnTest(true);
+  };
+
+  return (
+    <div className={styles.contentArea}>
+      <div className={styles.loadTestInfo}>
+        {/* TODO : loadTest? 제거 */}
+
+        <div>
+          <h2>{loadTest?.name}</h2>
+          <p>{loadTest?.description}</p>
+        </div>
+        <button className={buttonStyles.container} onClick={handleSelectExcute}>
+          <img src={playIcon} alt="excute loadtest" width={24} height={24} />
+        </button>
+      </div>
+
+      <ChartArea loadTest={loadTest} onTest={onTest} />
+    </div>
+  );
+};
+
+export default LoadTestInfo;
