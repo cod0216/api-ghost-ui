aee0cc758844878d76b09199b6788a71a300aaa8
haelim-kweon <khaelim1311@gmail.com>
2025-05-22 00:28:41 +0900

    chore: Modify API path

--- src/common/service/api.ts
+++ src/common/service/api.ts
@@ -1,8 +1,7 @@
 import { HttpMethod, RequestOptions } from '@/common/types';
 import { buildQueryString, buildHeaders, buildFetchOptions } from '@/common/utils/apiUtils';
 
-let baseUrl = 'http://localhost:7000';
-// let baseUrl = 'http://ec2-3-38-110-166.ap-northeast-2.compute.amazonaws.com:7000';
+let baseUrl = '';
 
 export const request = async <T = any>(
   url: string,
--- src/pages/flow-canvas/service/scenarioService.ts
+++ src/pages/flow-canvas/service/scenarioService.ts
@@ -29,8 +29,7 @@ export const exportScenario = async (
 
 export const scenarioTest = (name: string): EventSource => {
   const queryString = new URLSearchParams({ scenarioName: name }).toString();
-  const fullUrl = `http://localhost:7000/apighost/scenario-test?${queryString}`;
-  //http://ec2-3-38-110-166.ap-northeast-2.compute.amazonaws.com:7000
+  const fullUrl = `/apighost/scenario-test?${queryString}`;
   return new EventSource(fullUrl);
 };
 
